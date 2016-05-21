be6f31353719a0c3a9a46dc3abf0e51c19dbebf4
awk888 <awk888@users.noreply.github.com>
2016-05-20 19:30:26 -0700

    Update main-highlitghter.js

--- main-highlitghter.js
+++ main-highlitghter.js
@@ -4,7 +4,7 @@
 
 // The patterns for the JavaScript code.
 var find_function = /var|function|return|new|for/g;
-var find_string = /\"([^\"]+)\"/g;
+var find_string = /\"([^\"]+)\"|\'([^\']+)\'/g;
 var find_parameter = /\(([^\(]+)\(/g;
 var find_keyword = /this|RegExp|Object|prototype|true|false|document|Date|undefined|null|Array|Math|JSON|try|default|break|throw/g;
 var find_operator = /^\s*(?:\+|\-|\*|\\)/g;
